b09cebb2b0840e0bb0050f44b4d386905e5ceb4f
geongun20 <leesg9@snu.ac.kr>
2023-08-20 03:07:27 +0900

    fix: CustomAppBar status bar color

--- lib/app/presentation/widgets/global_widget.dart
+++ lib/app/presentation/widgets/global_widget.dart
@@ -1,5 +1,6 @@
 import 'dart:ui';
 
+import 'package:flutter/services.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:yummy_quest/core/themes/color_theme.dart';
@@ -9,45 +10,47 @@ class CustomAppBar extends StatelessWidget implements PreferredSizeWidget {
 
   @override
   Widget build(BuildContext context) {
-    return ClipRRect(
-      child: BackdropFilter(
-        filter: ImageFilter.blur(sigmaX: 16, sigmaY: 16),
-        child: SafeArea(
-          child: Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Row(
-              children: [
-                Container(
-                  height: 48,
-                  width: 48,
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(8),
-                    color: MyColors.Dim200,
-                  ),
-                  child: IconButton(
-                    onPressed: Get.back,
-                    icon: Icon(Icons.arrow_back),
-                  ),
+    return AnnotatedRegion<SystemUiOverlayStyle>(
+        value: SystemUiOverlayStyle.dark,
+        child: ClipRRect(
+          child: BackdropFilter(
+            filter: ImageFilter.blur(sigmaX: 16, sigmaY: 16),
+            child: SafeArea(
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Row(
+                  children: [
+                    Container(
+                      height: 48,
+                      width: 48,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(8),
+                        color: MyColors.Dim200,
+                      ),
+                      child: IconButton(
+                        onPressed: Get.back,
+                        icon: Icon(Icons.arrow_back),
+                      ),
+                    ),
+                    Spacer(),
+                    Container(
+                      height: 48,
+                      width: 48,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(8),
+                        color: MyColors.Dim200,
+                      ),
+                      child: IconButton(
+                        onPressed: Get.back,
+                        icon: Icon(Icons.ios_share_outlined),
+                      ),
+                    ),
+                  ],
                 ),
-                Spacer(),
-                Container(
-                  height: 48,
-                  width: 48,
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(8),
-                    color: MyColors.Dim200,
-                  ),
-                  child: IconButton(
-                    onPressed: Get.back,
-                    icon: Icon(Icons.ios_share_outlined),
-                  ),
-                ),
-              ],
+              ),
             ),
           ),
-        ),
-      ),
-    );
+        ));
   }
 
   @override
