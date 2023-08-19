3d2385720af74348996f5daf3e0706f82f5e5282
hanjoongyoo <hanjoongyoo@naver.com>
2023-08-19 20:30:01 +0900

    chore: 디자인 시스템 옮기는 작업

---
+++ lib/core/themes/color_theme.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+abstract class MyColors {
+  static const Black = Color(0xFF0F0F10);
+  static const Dim900 = Color(0xE60F0F10);
+  static const Dim800 = Color(0xD91B1B1C);
+  static const Dim700 = Color(0xBF2A2A2C);
+  static const Dim600 = Color(0x993B3B3D);
+  static const Dim500 = Color(0x99737376);
+  static const Dim400 = Color(0x99929295);
+  static const Dim300 = Color(0xBFD1D1D2);
+  static const Dim200 = Color(0xBFE5E5E6);
+  static const Dim100 = Color(0xD9FDFDFE);
+  static const Gray900 = Color(0xFF1B1B1C);
+  static const Gray800 = Color(0xFF2A2A2C);
+  static const Gray700 = Color(0xFF3B3B3D);
+  static const Gray600 = Color(0xFF525254);
+  static const Gray500 = Color(0xFF737376);
+  static const Gray400 = Color(0xFF929295);
+  static const Gray300 = Color(0xFFBDBDBF);
+  static const Gray200 = Color(0xFFD1D1D2);
+  static const Gray100 = Color(0xFFE5E5E7);
+  static const Gray50 = Color(0xFFF3F3F4);
+  static const Blue = Color(0xFF6966F4);
+  static const Orange = Color(0xFFF36942);
+  static const PaleGreen = Color(0xFFDDF8EB);
+  static const Yellow = Color(0xFFFAE5A1);
+  static const Red = Color(0xFFF46058);
+  static const PaleYellow = Color(0xFFFFF9E5);
+  static const Green = Color(0xFF1B7047);
+  static const LightYellow = Color(0xFFFFF0C1);
+  static const White = Color(0xFFFEFEFF);
+}
---
+++ lib/core/themes/text_theme.dart
@@ -0,0 +1,84 @@
+import 'package:flutter/cupertino.dart';
+
+abstract class MyTextStyles {
+  static const Giant_w400 = TextStyle(
+    fontSize: 24,
+    height: 36/24,
+    fontWeight: FontWeight.w400,
+  );
+  static const Giant_w600 = TextStyle(
+    fontSize: 24,
+    height: 36/24,
+    fontWeight: FontWeight.w600,
+  );
+  static const Giant_w800 = TextStyle(
+    fontSize: 24,
+    height: 36/24,
+    fontWeight: FontWeight.w800,
+  );
+  static const Large_w400 = TextStyle(
+    fontSize: 20,
+    height: 28/20,
+    fontWeight: FontWeight.w400,
+  );
+  static const Large_w600 = TextStyle(
+    fontSize: 20,
+    height: 28/20,
+    fontWeight: FontWeight.w600,
+  );
+  static const Large_w800 = TextStyle(
+    fontSize: 20,
+    height: 28/20,
+    fontWeight: FontWeight.w800,
+  );
+  static const Medium_w400 = TextStyle(
+    fontSize: 16,
+    height: 24/16,
+    fontWeight: FontWeight.w400,
+  );
+  static const Medium_w600 = TextStyle(
+    fontSize: 16,
+    height: 24/16,
+    fontWeight: FontWeight.w600,
+  );
+  static const Medium_w800 = TextStyle(
+    fontSize: 16,
+    height: 24/16,
+    fontWeight: FontWeight.w800,
+  );
+  static const Small_w400 = TextStyle(
+    fontSize: 13,
+    height: 16/13,
+    fontWeight: FontWeight.w400,
+  );
+  static const Small_w600 = TextStyle(
+    fontSize: 13,
+    height: 16/13,
+    fontWeight: FontWeight.w600,
+  );
+  static const Small_w800 = TextStyle(
+    fontSize: 13,
+    height: 16/13,
+    fontWeight: FontWeight.w800,
+  );
+  static const Tiny_w400 = TextStyle(
+    fontSize: 11,
+    height: 16/11,
+    fontWeight: FontWeight.w400,
+  );
+  static const Tiny_w600 = TextStyle(
+    fontSize: 11,
+    height: 16/11,
+    fontWeight: FontWeight.w600,
+  );
+  static const Tiny_w800 = TextStyle(
+    fontSize: 11,
+    height: 16/11,
+    fontWeight: FontWeight.w800,
+  );
+  static const Giant = TextStyle(
+    fontSize: 11,
+    height: 16/11,
+    fontWeight: FontWeight.w800,
+  );
+}
